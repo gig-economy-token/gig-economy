6bd02230be62b02fe9270dd822a1eb895b2de2ab
Javier Casas <javcasas@gmail.com>
2019-01-30 15:15:35 -0500

    WIP

--- src/Application.hs
+++ src/Application.hs
@@ -46,6 +46,7 @@ import Handler.Comment
 import Handler.Profile
 
 import CardanoStuff ()
+import Cardano.GameContract ()
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
