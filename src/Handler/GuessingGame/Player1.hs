28c8197fee2408683a7fac32ceefbe71a29c7c06
Javier Casas <javcasas@gmail.com>
2019-02-11 13:20:20 -0500

    Working on player 2 actions

--- src/Handler/GuessingGame/Player1.hs
+++ src/Handler/GuessingGame/Player1.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE QuasiQuotes        #-}
 module Handler.GuessingGame.Player1
   ( getPlayer1R
-  , postPlayer1DepositFundsR
   , postPlayer1StartGameR
   , postPlayer1GuessR
   ) where
@@ -14,17 +13,15 @@ import Cardano.Helpers
 import qualified Wallet.API as WalletAPI
 import qualified Wallet.Emulator as Emulator
 import Cardano.Html.Emulator
+import Cardano.Emulator.GuessingGame
 import qualified Data.Map as Map
-
-wallet1 :: Emulator.Wallet
-wallet1 = Emulator.Wallet 1
 
 renderLayout :: Html -> Html -> Handler Html
 renderLayout action content = do
     emulatorState <- readEmulatorState
     let
         fundsInWallet1 :: Int
-        fundsInWallet1 = fromMaybe 0 $ getResultingFunds <$> Map.lookup wallet1 (Emulator._walletStates emulatorState)
+        fundsInWallet1 = fromMaybe 0 $ getResultingFunds <$> Map.lookup player1Wallet (Emulator._walletStates emulatorState)
     defaultLayout $ do
         setTitle "Player 1"
         let status = [shamlet|
@@ -35,13 +32,6 @@ renderLayout action content = do
 
 getPlayer1R :: Handler Html
 getPlayer1R = renderLayout "Status" "Player 1 starts the game and tries to guess"
-
-postPlayer1DepositFundsR :: Handler Html
-postPlayer1DepositFundsR = do
-  error "Not implemented yet"
-  --appendStep $ _f $ Emulator.liftMockWallet wallet1 (WalletAPI.submitTxn $ createMiningTransaction [(wallet1, 50)])
-  --_ <- appendStep $ (Emulator.processPending >>= Emulator.walletsNotifyBlock [wallet1] >>= \x -> pure ())
-  renderLayout "- Mine funds" "Player1 mined 50 nanoADA"
 
 postPlayer1StartGameR :: Handler Html
 postPlayer1StartGameR = renderLayout "- Start game" "FIXME: Start Game"
--- src/Handler/GuessingGame/Player2.hs
+++ src/Handler/GuessingGame/Player2.hs
@@ -1,26 +1,33 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.GuessingGame.Player2 where
 
 import Import
 
 import qualified Wallet.Emulator as Emulator
 import qualified Cardano.Html.Emulator as CardanoHtml
-import qualified Cardano.Html.Template as CardanoHtml
+import qualified Data.Map as Map
+import Cardano.Helpers
+import Cardano.Emulator.GuessingGame
 
-getPlayer2R :: Handler Html
-getPlayer2R = do
+renderLayout :: Html -> Html -> Handler Html
+renderLayout action content = do
     emulatorState <- CardanoHtml.readEmulatorState
     defaultLayout $ do
         setTitle "Player 2 status"
         let
-            contentTitle = "FIXME" :: String
-            content = "FIXME" :: String
+            fundsInWallet2 = fromMaybe 0 $ getResultingFunds <$> Map.lookup player2Wallet (Emulator._walletStates emulatorState)
+            status = [shamlet|
+<ul>
+  <li>Funds in wallet: #{fundsInWallet2}
+|]
 
         $(widgetFile "guessing-game/player2")
 
-postPlayer2DepositFundsR :: Handler Html
-postPlayer2DepositFundsR = undefined
+
+getPlayer2R :: Handler Html
+getPlayer2R = renderLayout "Status" "Player 2 locks some funds"
 
 postPlayer2LockR :: Handler Html
 postPlayer2LockR = undefined
