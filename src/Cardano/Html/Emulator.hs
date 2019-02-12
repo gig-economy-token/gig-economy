6e9251a5fc89a99c08bd3dcdf074c22396ce8231
Javier Casas <javcasas@gmail.com>
2019-02-12 16:46:58 -0500

    Cleaned up emulator

--- src/Cardano/Html/Emulator.hs
+++ src/Cardano/Html/Emulator.hs
@@ -1,49 +1,54 @@
 {-# LANGUAGE NoImplicitPrelude    #-}
-{-# LANGUAGE ConstraintKinds      #-}
 {-# LANGUAGE FlexibleContexts     #-}
 {-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 
 module Cardano.Html.Emulator
   ( HasSimulatedChain(..)
   , appendStepAndNotifyKnownWallets
+  , appendStep
+  , readEmulatorState
   ) where
 
 import Import
 import Yesod.Core.Types
+import Wallet.Emulator
 
-import qualified Wallet.Emulator as Emulator
 import Cardano.Emulator
 
-
 class Monad m => HasSimulatedChain m where
-  readSimulatedChain              :: m SimulatedChain
-  readEmulatorState               :: m Emulator.EmulatorState
-  appendStep                      :: Emulator.Trace Emulator.MockWallet () -> m ()
+  readSimulatedChain    :: m SimulatedChain
+  modifySimulatedChain  :: (SimulatedChain -> SimulatedChain) -> m ()
 
 instance HasSimulatedChain Handler where
   readSimulatedChain = readSimulatedChainRef >>= readIORef
-  readEmulatorState = scEmulatorState <$> readSimulatedChain
-  appendStep newStep = do
-                        scRef <- readSimulatedChainRef
-                        sc <- readIORef scRef
-                        let prevTrace = scTrace sc
-                            newTrace = prevTrace >> newStep
-                            (_, newEmulatorState) = Emulator.runTraceTxPool initialTx newTrace
-                            sc' = SimulatedChain
-                                      { scEmulatorState = newEmulatorState
-                                      , scTrace = newTrace
-                                      }
-                        atomicModifyIORef' scRef (\_ -> (sc', ()))
+  modifySimulatedChain f = do
+                            scRef <- readSimulatedChainRef
+                            atomicModifyIORef' scRef (\sc -> (f sc, ()))
 
-readSimulatedChainRef :: MonadReader (HandlerData c App) m => m (IORef SimulatedChain)
-readSimulatedChainRef = (simulatedChain . rheSite . handlerEnv) <$> ask
+readEmulatorState :: HasSimulatedChain m => m EmulatorState
+readEmulatorState = scEmulatorState <$> readSimulatedChain
 
--- Helper for most cases
-appendStepAndNotifyKnownWallets :: HasSimulatedChain m => Emulator.Trace Emulator.MockWallet () -> m ()
+appendStep :: HasSimulatedChain m => Trace MockWallet () -> m ()
+appendStep newStep = modifySimulatedChain f
+  where
+    f sc = sc'
+      where
+        prevTrace = scTrace sc
+        newTrace = prevTrace >> newStep
+        (_, newEmulatorState) = runTraceTxPool initialTx newTrace
+        sc' = SimulatedChain
+                { scEmulatorState = newEmulatorState
+                , scTrace = newTrace
+                }
+
+appendStepAndNotifyKnownWallets :: HasSimulatedChain m => Trace MockWallet () -> m ()
 appendStepAndNotifyKnownWallets newStep = appendStep stepAndNotify
   where
     stepAndNotify = do
                     newStep
-                    _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock allKnownWallets
+                    _ <- processPending >>= walletsNotifyBlock allKnownWallets
                     pure ()
+
+-- Only for HasSimulatedChain Handler
+readSimulatedChainRef :: MonadReader (HandlerData c App) m => m (IORef SimulatedChain)
+readSimulatedChainRef = (simulatedChain . rheSite . handlerEnv) <$> ask
