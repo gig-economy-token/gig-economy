1ac6a2d0db9adf1e9dbd4c82e51f8f493173c129
Javier Casas <javcasas@gmail.com>
2019-02-26 16:51:52 -0500

    Client wants property tests (and proofs, but those are harder)

--- test/Cardano/JobContractSpec.hs
+++ test/Cardano/JobContractSpec.hs
@@ -2,14 +2,16 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Cardano.JobContractSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import qualified Ledger
 import Cardano.JobContract
-import ArbitraryInstances ()
+import ArbitraryInstances
 import qualified Wallet.Emulator as Emulator
+import qualified Wallet.Emulator.AddressMap as AM
 import Cardano.Helpers
 import Data.Either
 import qualified Data.Map as Map
@@ -29,29 +31,32 @@ spec = do
           jobAcceptance' = parseJobAcceptance datascript
       jobAcceptance' `shouldBe` Just jobAcceptance
 
+  describe "toJobOffer - toJobOfferForm isomorphism" $ do
+    prop "->" $ \(jof, k) -> (toJobOfferForm $ toJobOffer jof k) `shouldBe` jof
+    prop "<-" $ \jo -> (toJobOffer (toJobOfferForm jo) (joOfferer jo)) `shouldBe` jo
+
   describe "Basic use cases" $ do
-    it "Employer posts a job, employee reads it back" $ do
-      let wallets@[wEmployer, wEmployee] = Emulator.Wallet <$> [1, 2]
+    prop "Employer posts a job, employee reads it back" $
+      \(Different (employeePk, employerPk), jobOfferForm) -> do
+      let wallets@[wEmployer, wEmployee] = walletFromPubKey <$> [employerPk, employeePk]
           initialTx = createMiningTransaction [(wEmployer, 1)]
-          jobOffer = JobOffer "Description" 4 (Ledger.PubKey 1)
+          jobOffer = toJobOffer jobOfferForm employerPk
           (result, state) = Emulator.runTraceTxPool [initialTx] $ do
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
               _ <- Emulator.walletAction wEmployee $ subscribeToJobBoard
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
-              _ <- Emulator.walletAction wEmployer $ postOffer (toJobOfferForm jobOffer)
+              _ <- Emulator.walletAction wEmployer $ postOffer jobOfferForm
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
               pure ()
-          walletStates = Emulator._walletStates state
-          Just wsEmployee = Map.lookup wEmployee walletStates
-          am = Emulator._addressMap wsEmployee
 
       result `shouldSatisfy` isRight
-      extractJobOffers am `shouldBe` Just [jobOffer]
+      getEmulatorErrors state `shouldBe` []
+      extractJobOffers (getAddressMap state wEmployee) `shouldBe` Just [jobOffer]
 
-    it "Employer posts a job, then closes it, employee can't read it anymore" $ do
-      let wallets@[wEmployer, wEmployee] = Emulator.Wallet <$> [2001, 2002]
+    prop "Employer posts a job, then closes it, employee can't read it anymore" $
+      \(Different (employeePk, employerPk), jobOfferForm) -> do
+      let wallets@[wEmployee, wEmployer] = walletFromPubKey <$> [employeePk, employerPk]
           initialTx = createMiningTransaction [(wEmployer, 1)]
-          jobOfferForm = JobOfferForm "Description" 4
           (result, state) = Emulator.runTraceTxPool [initialTx] $ do
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
               _ <- Emulator.walletAction wEmployee $ subscribeToJobBoard
@@ -61,35 +66,42 @@ spec = do
               _ <- Emulator.walletAction wEmployer $ closeOffer jobOfferForm
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
               pure ()
-          walletStates = Emulator._walletStates state
-          Just wsEmployee = Map.lookup wEmployee walletStates
-          am = Emulator._addressMap wsEmployee
 
-          getValidationFail :: Emulator.EmulatorEvent -> Maybe (Ledger.TxId, Ledger.ValidationError)
-          getValidationFail (Emulator.TxnValidationFail a b) = Just (a, b)
-          getValidationFail _ = Nothing
-
-      (catMaybes $ getValidationFail <$> (Emulator._emulatorLog state)) `shouldBe` []
       result `shouldSatisfy` isRight
-      extractJobOffers am `shouldBe` Just []
+      getEmulatorErrors state `shouldBe` []
+      extractJobOffers (getAddressMap state wEmployee) `shouldBe` Just []
 
-    it "Employer posts a job, employee accepts it, employer sees acceptance" $ do
-      let wallets@[wEmployer, wEmployee] = Emulator.Wallet <$> [1, 2]
+    prop "employer posts a job, employee accepts it, employer sees acceptance" $
+      \(Different (employeePk, employerPk), jobOfferForm) -> do
+      let wallets@[wEmployer, wEmployee] = walletFromPubKey <$> [employerPk, employeePk]
           initialTx = createMiningTransaction [(wEmployer, 1)]
-          jobOffer = JobOffer "Description" 4 (Ledger.PubKey 1)
+          jobOffer = toJobOffer jobOfferForm employerPk
           (result, state) = Emulator.runTraceTxPool [initialTx] $ do
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
               _ <- Emulator.walletAction wEmployee $ subscribeToJobBoard
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
-              _ <- Emulator.walletAction wEmployer $ postOffer (toJobOfferForm jobOffer)
+              _ <- Emulator.walletAction wEmployer $ postOffer jobOfferForm
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
               _ <- Emulator.walletAction wEmployee $ acceptOffer jobOffer
               _ <- Emulator.processPending >>= Emulator.walletsNotifyBlock wallets
               pure ()
-          walletStates = Emulator._walletStates state
-          Just wsEmployer = Map.lookup wEmployer walletStates
-          am = Emulator._addressMap wsEmployer
-          acceptances = extractJobAcceptances am jobOffer
 
       result `shouldSatisfy` isRight
-      acceptances `shouldBe` Just [JobAcceptance (Ledger.PubKey 2)]
+      getEmulatorErrors state `shouldBe` []
+      extractJobAcceptances (getAddressMap state wEmployer) jobOffer `shouldBe` Just [JobAcceptance employeePk]
+
+walletFromPubKey :: Ledger.PubKey -> Emulator.Wallet
+walletFromPubKey (Ledger.PubKey x) = Emulator.Wallet x
+
+getAddressMap :: Emulator.EmulatorState -> Emulator.Wallet -> AM.AddressMap
+getAddressMap state wallet = Emulator._addressMap walletState
+  where
+    walletStates = Emulator._walletStates state
+    Just walletState = Map.lookup wallet walletStates
+
+getEmulatorErrors :: Emulator.EmulatorState -> [Ledger.ValidationError]
+getEmulatorErrors state = catMaybes $ getValidationFail <$> (Emulator._emulatorLog state)
+  where
+    getValidationFail :: Emulator.EmulatorEvent -> Maybe Ledger.ValidationError
+    getValidationFail (Emulator.TxnValidationFail _ b) = Just b
+    getValidationFail _ = Nothing
