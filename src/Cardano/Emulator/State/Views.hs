6d360eb2827baa6f064bc6c5b768a4117c740a1f
Sebastián Estrella <sestrella.me@gmail.com>
2019-04-04 23:34:02 -0500

    [24] Present blockchain status in a different format using tables instead of plain text

---
+++ src/Cardano/Emulator/State/Views.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Cardano.Emulator.State.Views
+  ( statusW
+  ) where
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Import as I
+
+import Cardano.Helpers
+import Ledger.Types
+import Ledger.Value.TH
+import Wallet.Emulator
+
+statusW :: EmulatorState -> I.Widget
+statusW EmulatorState{..} = $(I.widgetFile "status")
+
+walletsW :: M.Map Wallet WalletState -> I.Widget
+walletsW wallets = $(I.widgetFile "status/wallets")
+
+blocksW :: Blockchain -> I.Widget
+blocksW blocks = $(I.widgetFile "status/blocks")
+
+transactionsW :: Tx -> I.Widget
+transactionsW Tx{..} = $(I.widgetFile "status/transactions")
+
+inputsW :: [TxIn] -> I.Widget
+inputsW ins = $(I.widgetFile "status/inputs")
+
+outputsW :: [TxOut] -> I.Widget
+outputsW outs = $(I.widgetFile "status/outputs")
+
+logsW :: [EmulatorEvent] -> I.Widget
+logsW logs = $(I.widgetFile "status/logs")
+
+logW :: EmulatorEvent -> I.Widget
+logW (SlotAdd s)                  = $(I.widgetFile "status/log/slot-add")
+logW (TxnSubmit txId)             = $(I.widgetFile "status/log/txn-submit")
+logW (TxnValidate txId)           = $(I.widgetFile "status/log/txn-validate")
+logW (TxnValidationFail txId err) = $(I.widgetFile "status/log/txn-validation-fail")
+logW (WalletError w err)          = $(I.widgetFile "status/log/wallet-error")
+logW (WalletInfo w info)          = $(I.widgetFile "status/log/wallet-info")
+
+txIdToString :: TxId -> String
+txIdToString (TxIdOf txId) = show txId
+
+values :: Value -> Int
+values = sum . map snd . getValue
