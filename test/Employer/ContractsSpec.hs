ae6ee2bc5efe06f4c51be0f83a6420412501ca65
Sebastián Estrella <sestrella.me@gmail.com>
2019-04-11 09:42:10 -0500

    Test job offer funds

--- test/Employer/ContractsSpec.hs
+++ test/Employer/ContractsSpec.hs
@@ -12,25 +12,45 @@ spec :: Spec
 spec =
   describe "postJobOffer" $
     it "creates a job offer with status opened" $ do
-      let (foo, bar) =
-            runTraceTxPool [] $ do
-              walletAction employerWallet $ do
-                let value = L.singleton (L.currencySymbol 1) 10
-                payToPublicKey_ defaultSlotRange value (PubKey 2)
+      let (foo, bar@EmulatorState{..}) =
+            runTraceTxPool initialTxPool $ do
+              processPending >>= walletsNotifyBlock wallets
 
+              walletAction employerWallet $
+                postJobOffer JobOffer
+                  { jobOfferId          = 1
+                  , jobOfferTitle       = "Foo"
+                  , jobOfferDescription = "Bar"
+                  , jobOfferPayout      = 20
+                  , jobOfferStatus      = Opened
+                  }
+
+              processPending >>= walletsNotifyBlock wallets
+
+              --   let value = L.singleton (L.currencySymbol 1) 10
+              --   payToPublicKey_ defaultSlotRange value (PubKey 1)
+
+      print foo
       print bar
 
-      foo `shouldSatisfy` isRight
+      _emulatorLog `shouldBe` []
 
+wallets :: [Wallet]
+wallets = [employerWallet, employeeWallet]
 
 employerWallet :: Wallet
 employerWallet = Wallet 1
 
+employeeWallet :: Wallet
+employeeWallet = Wallet 2
+
 initialTxPool :: TxPool
 initialTxPool = pure L.Tx
-  { txInputs = mempty
-  , txOutputs = mempty
-  , txForge = undefined
-  , txFee = fromInteger 0
-  , txValidRange = undefined
+  { txInputs     = mempty
+  , txOutputs    = pure $ L.pubKeyTxOut value (PubKey 1)
+  , txForge      = value
+  , txFee        = fromInteger 0
+  , txValidRange = defaultSlotRange
   }
+  where
+    value = L.singleton (L.currencySymbol 1) 10
